f3b78e186d9c5da446f937bca29ccdb46d35d3e9
David Ladipo <deedevs21@gmail.com>
2023-03-12 19:28:04 +0000

    header section

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,7 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
+import { images } from "../../images";
+import "./header.scss";
 
 const Header = () => {
-  return <div></div>;
+  const [open, setOpen] = useState(false);
+  const handleClick = (e) => {
+    e.preventDefault();
+    setOpen(!open);
+  };
+  return (
+    <div>
+      <header>
+        <nav className="navbar container">
+          <div className="logo">
+            <img src={images.logo}></img>
+          </div>
+          <ul className={open ? `nav-items active` : `nav-items`}>
+            <li>About</li>
+            <li>Contact</li>
+            <li>Portfolio</li>
+            <li className="btn btn--nav-btn">View Plans</li>
+          </ul>
+          <div className="hamburger">
+            <img src={images.hamburger} onClick={handleClick}></img>
+          </div>
+        </nav>
+      </header>
+    </div>
+  );
 };
 
 export default Header;
